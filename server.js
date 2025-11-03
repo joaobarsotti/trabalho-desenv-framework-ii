af73229348c570a8790928e7384ba8af9e5adf3e
Joao Pedro <joao.barsotti@hotmail.com>
2025-11-03 13:21:40 -0300

    Atualizações finais

--- server.js
+++ server.js
@@ -19,16 +19,11 @@ const { User, Equipment, Maintenance } = db;
 
 const app = express();
 
-//const PORT = 3000; 
-
 const PORT = process.env.PORT || 8080;
-
 
 app.use(cors()); 
 app.use(express.json());
 app.use(express.static(__dirname));
-
-
 app.use(express.static(path.resolve(__dirname)));
 
 const swaggerDocument = YAML.load('./swagger.yaml');
